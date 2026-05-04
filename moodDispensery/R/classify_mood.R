b30c5315cc95e5ba2e37672b673e4735cb3d39df
Grace <gwhitehurst@usf.edu>
2026-05-03 23:39:40 -0400

    Add classify_mood function to classify sentiments

--- moodDispensery/R/classify_mood.R
+++ moodDispensery/R/classify_mood.R
@@ -1,1 +1,12 @@
+classify_mood <- function(sentiment_df) {
+
+  sentiment_df %>%
+    mutate(
+      mood = case_when(
+        score > 0 ~ "happy",
+        score < 0 ~ "sad",
+        TRUE ~ "neutral"
+      )
+    )
+}
 
